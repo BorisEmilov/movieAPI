f6f430fc7ef7ccea4ee3038f5c2b3121d4c68dcd
Boris Emilov <bobi.emilov1@gmail.com>
2023-06-04 13:39:48 +0200

    03

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const request = require('request')
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = (process.env.PORT || 5000);
 
 app.get('/', (req, res) => {
     res.send('Wellcome to amazon api')
